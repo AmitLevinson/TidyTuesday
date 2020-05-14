dc7d87fdcd3b8eb4b008762399f2da593200aff2
AmitLevinson <amitlevinson1@gmail.com>
2020-05-14 13:51:05 +0300

    edit dpi for twitter

--- 2020/week20_volcano/week20_volcano.R
+++ 2020/week20_volcano/week20_volcano.R
@@ -42,16 +42,18 @@ volcano_loc_n %>%
   annotate(geom = "curve", x = 45, xend = 72, y = 191, yend = 196,
            curvature = -.2,color = "grey35", size = 0.75, arrow = arrow(length = unit(1.5, "mm")))+
   annotate("text", x = 42, y = 180, label = "Etna (Italy), one of the world's most\nactive volcanoes, erupted 196 times. Today\n78 people live within 5k.",
-           color = "grey10", hjust = 0, size = 5, family = "JetBrains Mono")+
+           color = "grey20", hjust = 0, size = 5, family = "JetBrains Mono")+
   annotate(geom = "curve", x = 105000, xend =	59000, y = 190, yend =189,
            curvature = .2, color = "grey35", size = 0.75, arrow = arrow(length = unit(1.5, "mm")))+ # Fournaise
   annotate(geom = "curve", x = 105000, xend = 80000, y = 179, yend = 171,
            curvature = -.2, color = "grey35", size = 0.75, arrow = arrow(length = unit(1.5, "mm")))+ # Asosan
   annotate("text", x = 111000, y = 187, label = "Piton de la Fournaise (top) and\nAsosan (bottom) are both highly\nactive volcanoes that erupted ~190\ntimes. Today some 55,000 people\nlive within 5k.",
-           color = "grey10", hjust = 0, family = "JetBrains Mono", size = 5)+
+           color = "grey20", hjust = 0, family = "JetBrains Mono", size = 5)+
   annotate(geom = "curve", x = 3000, xend = 4000, y = 70, yend = 9,
            curvature = .2, color = "grey35", size = 0.9, arrow = arrow(length = unit(1.5, "mm")))+
   annotate("text", x = 3000, y = 80, label = "Tambora's (Indonesia) 1815 eruption was the most\npowerful eruption recorded in human history.\nToday 4156 people live in a 5 kilomoter distance.",
-           color = "grey10", hjust = 0, size = 5, family = "JetBrains Mono")
+           color = "grey20", hjust = 0, size = 5, family = "JetBrains Mono")
 
-ggsave("vc.png", width = 16, height = 10, dpi = 1020)
+ggsave("vc2.png", width = 16, height = 10, dpi = 320)
+
+
