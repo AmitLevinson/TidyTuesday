c2cbb6ca527952b2ba88b19eff42f047caf7b12a
AmitLevinson <amitlevinson1@gmail.com>
2020-05-14 13:33:38 +0300

    update size

--- 2020/week20_volcano/week20_volcano.R
+++ 2020/week20_volcano/week20_volcano.R
@@ -29,7 +29,7 @@ volcano_loc_n %>%
   theme(text = element_text(family = "Roboto Condensed"),
         #plot.title = element_markdown(color = "#FB9A99"),
     plot.title = element_text(face = "bold", size = 45),
-    plot.subtitle = element_text(size = 14, color = "grey10"),
+    plot.subtitle = element_text(size = 16, color = "grey10"),
     plot.caption = element_text(size = 9, color = "grey25"),
     plot.background = element_rect(fill = "grey50"),
     panel.background = element_rect(fill = "grey50"),
@@ -39,19 +39,20 @@ volcano_loc_n %>%
     axis.ticks = element_blank(),
     plot.margin = margin(4,4,2,4, unit = "mm")
   )+
-  annotate(geom = "curve", x = 45, xend = 72, y = 190, yend = 196,
+  annotate(geom = "curve", x = 45, xend = 72, y = 191, yend = 196,
            curvature = -.2,color = "grey35", size = 0.75, arrow = arrow(length = unit(1.5, "mm")))+
-  annotate("text", x = 42, y = 181, label = "Etna (Italy), one of the world's most\nactive volcanoes, erupted 196 times. Today\n78 people live within 5k.",
-           color = "grey10", hjust = 0, size = 4, family = "JetBrains Mono")+
+  annotate("text", x = 42, y = 180, label = "Etna (Italy), one of the world's most\nactive volcanoes, erupted 196 times. Today\n78 people live within 5k.",
+           color = "grey10", hjust = 0, size = 5, family = "JetBrains Mono")+
   annotate(geom = "curve", x = 105000, xend =	59000, y = 190, yend =189,
            curvature = .2, color = "grey35", size = 0.75, arrow = arrow(length = unit(1.5, "mm")))+ # Fournaise
   annotate(geom = "curve", x = 105000, xend = 80000, y = 179, yend = 171,
            curvature = -.2, color = "grey35", size = 0.75, arrow = arrow(length = unit(1.5, "mm")))+ # Asosan
-  annotate("text", x = 111000, y = 186, label = "Piton de la Fournaise (top) and Asosan (bottom)\nare both highly active volcanoes that erupted ~190\ntimes. Today some 55,000 people live within 5k.",
-           color = "grey10", hjust = 0, family = "JetBrains Mono", size = 4)+
-  annotate(geom = "curve", x = 3000, xend = 4000, y = 72, yend = 9,
+  annotate("text", x = 111000, y = 187, label = "Piton de la Fournaise (top) and\nAsosan (bottom) are both highly\nactive volcanoes that erupted ~190\ntimes. Today some 55,000 people\nlive within 5k.",
+           color = "grey10", hjust = 0, family = "JetBrains Mono", size = 5)+
+  annotate(geom = "curve", x = 3000, xend = 4000, y = 70, yend = 9,
            curvature = .2, color = "grey35", size = 0.9, arrow = arrow(length = unit(1.5, "mm")))+
   annotate("text", x = 3000, y = 80, label = "Tambora's (Indonesia) 1815 eruption was the most\npowerful eruption recorded in human history.\nToday 4156 people live in a 5 kilomoter distance.",
-           color = "grey10", hjust = 0, size = 4, family = "JetBrains Mono")
+           color = "grey10", hjust = 0, size = 5, family = "JetBrains Mono")
 
-ggsave("vc.png", width = 18, height = 10, dpi = 1020)
+ggsave("vc.png", width = 16, height = 10, dpi = 1020)
+setwd("2020/week20_volcano")
