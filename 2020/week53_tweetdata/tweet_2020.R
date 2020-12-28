7647130d789ffdf027fc0c9cbdccdf19369e1929
AmitLevinson <amitlevinson1@gmail.com>
2020-12-28 19:26:13 +0200

    fix itle

--- 2020/week53_tweetdata/tweet_2020.R
+++ 2020/week53_tweetdata/tweet_2020.R
@@ -43,7 +43,7 @@ week_data_full <- left_join(week_count, week_name, by = c("week_count" = "Week")
 
 ggplot(week_data_full)+
   geom_col(aes(y = fct_reorder(name, n), x = n, fill = al_participated), show.legend = FALSE)+
-  labs(title = "Top 15 #Tidytuesday weeks",
+  labs(title = "#Tidytuesday weeks with the most contributions",
        subtitle = "Only tweets containing photos were aggregated. <b><span style='color:#453F78'>Highlighted are the weeks I participated in.</span></b><br>Caveat: individuals might\npost their analysis in following week from the original data.",
        x = "#TidyTuesday tweets with photos", y = "Data (Date)\n", caption = "Data: Tidytuesday & Thomas Mock\n visualizaiton: @Amit_Levinson")+
   scale_fill_manual(values = c("yes" = "#453F78", "no" = "gray55"))+
